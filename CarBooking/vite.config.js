eb85c625ee62af8e130d671478ea43d41f74b9d8
Dhivakar <dhivakarp305@gmail.com>
2026-06-09 17:56:09 +0530

    fix it

--- CarBooking/vite.config.js
+++ CarBooking/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         // target: "http://localhost:5000",
-        target: "https://printmy.qtechx.com",
+        target: "https://cars.qtechx.com",
         changeOrigin: true,
         secure: false,
       }
